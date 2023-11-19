985cb930b7a71c8eb22deada9ada169fe53ef8e8
mohammed tifour <mohamedtifourt562@gmail.com>
2023-11-19 18:49:12 +0100

    set up the app

---
+++ app.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+console.log(DB);
+mongoose.connect(DB).then(() => console.log('DATABASE CONNECTED...'));
+
+const port = process.env.PORT || 6000;
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
