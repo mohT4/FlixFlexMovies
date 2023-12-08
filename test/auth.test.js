cb1d0ab6899057dcf27ed612c63789aef4f5bfb4
mohammed tifour <mohamedtifourt562@gmail.com>
2023-12-08 16:25:04 +0100

    loggin test

--- test/auth.test.js
+++ test/auth.test.js
@@ -1,8 +1,8 @@
 const request = require('supertest');
 const app = require('../app');
-const logger = require('../middlewares/logger/logger');
 
 const setup = require('./setUp');
+const User = require('../models/user');
 
 describe('auth controller', () => {
   setup();
@@ -20,5 +20,21 @@ describe('auth controller', () => {
         .send(userData);
       expect(response.status).toBe(200);
     });
+  });
+
+  describe('logIn function', () => {});
+  it('should login a user', async () => {
+    const testUser = {
+      name: 'test user',
+      email: 'test@gmail.com',
+      password: 'password123456',
+      passwordConfirmation: 'password123456',
+    };
+    await User.create(testUser);
+    const user = {
+      email: 'test@gmail.com',
+      password: 'password123456',
+    };
+    await request(app).post('/api/v1/users/login').send(user).expect(200);
   });
 });
